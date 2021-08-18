32e8bee2736fdc2bb01da2949d4eac9aab4da28d
Timon Masberg <contact@timonmasberg.com>
2021-08-18 22:20:15 +0200

    Add Address structure

---
+++ address.go
@@ -0,0 +1,22 @@
+package address_formatter
+
+type Address struct {
+	HouseNumber   int
+	House         string
+	Road          string
+	Hamlet        string
+	Village       string
+	PostalCity    string
+	City          string
+	Municipality  string
+	County        string
+	StateDistrict string
+	Postcode      string
+	State         string
+	Region        string
+	Island        string
+	Archipelago   string
+	Country       string
+	CountryCode   string
+	Continent     string
+}
