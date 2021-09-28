adae7f25df027ee7cc796f01dcd2196811c04e8a
Timon Masberg <contact@timonmasberg.com>
2021-09-28 13:14:53 +0200

    Add address formatter to format addresses based on templates Add test suite that covers both formatting and address fixing due to coupling of test cases provided by OpenCageData

---
+++ address_formatter.go
@@ -0,0 +1,191 @@
+package address_formatter
+
+import (
+	"errors"
+	"fmt"
+	"github.com/cbroglie/mustache"
+	"html"
+	"log"
+	"regexp"
+	"strings"
+)
+
+var commonCountryCodeAliases = map[string]string{"UK": "GB"}
+var validReplacementComponents = []string{"state"}
+var requiredAddressProperties = []string{"road", "postcode"}
+
+var replacements = map[string]string{
+	`[\},\s]+$`:                  "",
+	`(?m)^[,\s]+`:                "",
+	`(?m)^- `:                    "",   // line starting with dash due to a parameter missing
+	`,\s*,`:                      ", ", // multiple commas to one
+	`[[:blank:]]+,[[:blank:]]+/`: ", ", // one horiz whitespace behind comma
+	`[[:blank:]][[:blank:]]+`:    " ",  // multiple horiz whitespace to one
+	`[[:blank:]]\n`:              "\n", // horiz whitespace, newline to newline
+	`\n,`:                        "\n", // newline comma to just newline
+	`,,+`:                        ",",  // multiple commas to one
+	`,\n`:                        "\n", // comma newline to just newline
+	`\n[[:blank:]]+`:             "\n", // newline plus space to newline
+	`\n\n+`:                      "\n", // multiple newline to one
+}
+
+// FormatAddress formats an Address object based on it
+func FormatAddress(address *Address, config *Config) (interface{}, error) {
+	// ease up the Address into a map to make it accessible via index
+	addressMap, err := addressToMap(address)
+
+	if err != nil {
+		return nil, err
+	}
+
+	template := findTemplate(address.CountryCode, config.Templates)
+	render, err := applyTemplate(addressMap, template, config.Templates)
+	if err != nil {
+		return nil, err
+	}
+
+	return getOutput(render, config.OutputFormat)
+}
+
+func applyTemplate(addressMap addressMap, template template, templates map[string]template) (string, error) {
+	templateText := chooseTemplateText(addressMap, template, templates)
+
+	render, _ := mustache.Render(templateText, getRenderInput(addressMap))
+	// unescape render to enforce official mustache HTML escaping rules
+	render = html.UnescapeString(render)
+	// todo: postformat replacements rely on a clean render but can mess it up again... (constraint by OpenCageData)
+	var err error
+	render, err = cleanupRender(render)
+	if err != nil {
+		return "", err
+	}
+	render = applyPostformatReplacements(render, template)
+	render, err = cleanupRender(render)
+	if err != nil {
+		return "", err
+	}
+
+	return render, nil
+}
+
+func getRenderInput(addressMap addressMap) map[string]interface{} {
+	input := make(map[string]interface{})
+
+	for k, v := range addressMap {
+		input[k] = v
+	}
+
+	input["first"] = func(t string, f func(string) (string, error)) (string, error) {
+		t, _ = f(t)
+		possibilities := regexp.MustCompile(`\s*\|\|\s*`).Split(t, -1)
+
+		for _, possibility := range possibilities {
+			if possibility != "" {
+				return possibility, nil
+			}
+		}
+
+		return "", nil
+	}
+
+	return input
+}
+
+func chooseTemplateText(address addressMap, template template, templates map[string]template) string {
+	var templateText string
+
+	templateValue, isTemplateMap := template.(map[string]interface{})
+
+	if isTemplateMap {
+		missingPropertyCount := 0
+		for _, requiredProperty := range requiredAddressProperties {
+			if _, hasProperty := address[requiredProperty]; !hasProperty {
+				missingPropertyCount++
+			}
+		}
+
+		if missingPropertyCount == len(requiredAddressProperties) {
+			if fallbackTemplate, hasFallbackTemplate := templateValue["fallback_template"]; hasFallbackTemplate {
+				templateText = fallbackTemplate.(string)
+			} else if defaultTemplate, hasDefaultTemplate := templates["default"]; hasDefaultTemplate {
+				templateText = defaultTemplate.(map[string]interface{})["fallback_template"].(string)
+			}
+		} else // has country specific template
+		if addressTemplate, hasAddressTemplate := templateValue["address_template"]; hasAddressTemplate {
+			templateText = addressTemplate.(string)
+		} else // has default template
+		if defaultTemplate, hasDefaultTemplate := templates["default"]; hasDefaultTemplate {
+			templateText = defaultTemplate.(map[string]interface{})["address_template"].(string)
+		}
+	} else {
+		templateText = template.(string)
+	}
+
+	return templateText
+}
+
+func cleanupRender(render string) (string, error) {
+	for regExp, replacement := range replacements {
+		r, err := regexp.Compile(regExp)
+
+		if err != nil {
+			return "", errors.New(fmt.Sprintf("Replace expression not compilable: %v", err))
+		}
+
+		render = r.ReplaceAllString(render, replacement)
+
+		render = dedupe(strings.Split(render, "\n"), "\n", func(s string) string {
+			return dedupe(strings.Split(s, ", "), ", ", func(s string) string {
+				return s
+			})
+		})
+	}
+
+	return strings.TrimSpace(render), nil
+}
+
+func dedupe(chunks []string, glue string, modifier func(s string) string) string {
+	seen := make(map[string]bool)
+	result := make([]string, 0)
+
+	for _, chunk := range chunks {
+		chunk = strings.TrimSpace(chunk)
+		if strings.ToLower(chunk) == "new york" {
+			seen[chunk] = true
+			result = append(result, chunk)
+		} else if seenChunk, hasChunk := seen[chunk]; !hasChunk || !seenChunk {
+			seen[chunk] = true
+			result = append(result, modifier(chunk))
+		}
+	}
+
+	return strings.Join(result, glue)
+}
+
+func applyPostformatReplacements(render string, template template) string {
+	if postformatReplacements, hasReplacements := template.(map[string]interface{})["postformat_replace"].([]interface{}); hasReplacements {
+		for _, replacement := range postformatReplacements {
+			r, err := regexp.Compile(replacement.([]interface{})[0].(string))
+			if err != nil {
+				log.Printf("Could not replace due to bad regexp: %v", err)
+			}
+
+			render = r.ReplaceAllString(render, replacement.([]interface{})[1].(string))
+		}
+	}
+
+	return render
+}
+
+func getOutput(render string, outputFormat OutputFormat) (interface{}, error) {
+	switch outputFormat {
+	case Array:
+		return strings.Split(render, "\n"), nil
+	case OneLine:
+		return strings.Replace(render, "\n", ", ", -1), nil
+	case PostalFormat:
+		return render + "\n", nil
+	default:
+		return nil, errors.New("invalid output format")
+	}
+}
---
+++ adress_formatter_test.go
@@ -0,0 +1,82 @@
+package address_formatter
+
+import (
+	"github.com/stretchr/testify/suite"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+// testing relies on testcases by OpenCageData (use testcases from submodule by executing copy-testcases.cmd or create your own)
+func TestAddressFormatterTestSuite(t *testing.T) {
+	suite.Run(t, new(FormatterTestSuite))
+}
+
+type FormatterTestSuite struct {
+	suite.Suite
+	Config    *Config
+	TestCases []*TestCase
+}
+type TestCase struct {
+	Name           string
+	Address        *Address
+	ExpectedOutput string
+}
+
+func (suite *FormatterTestSuite) SetupTest() {
+	// Load Config
+	suite.Config = LoadConfig(ConfigFiles{
+		CountriesPath:     "templates/countries/worldwide.yaml",
+		ComponentsPath:    "templates/components.yaml",
+		StateCodesPath:    "templates/state_codes.yaml",
+		CountryToLangPath: "templates/country2lang.yaml",
+		CountyCodesPath:   "templates/county_codes.yaml",
+		CountryCodesPath:  "templates/country_codes.yaml",
+		AbbreviationFiles: "templates/abbreviations/*.yaml",
+	})
+	suite.Config.OutputFormat = PostalFormat
+	suite.Config.UnknownAsAttention = true
+
+	// Load TestCases
+	testFiles, _ := filepath.Glob("testcases/*.yaml")
+
+	for _, filePath := range testFiles {
+		fileContent, _ := ioutil.ReadFile(filePath)
+		fileContentStr := string(fileContent)
+
+		var testCaseParts []string
+		if strings.Count(fileContentStr, "---") > 1 {
+			testCaseParts = strings.Split(fileContentStr, "---")[1:]
+		} else {
+			testCaseParts = append(testCaseParts, fileContentStr)
+		}
+		for _, testCasePart := range testCaseParts {
+			var parsedFileContent struct {
+				Components map[string]string `yaml:"components"`
+				Expected   string            `yaml:"expected"`
+			}
+
+			yaml.Unmarshal([]byte(testCasePart), &parsedFileContent)
+
+			testCase := new(TestCase)
+
+			testCase.ExpectedOutput = parsedFileContent.Expected
+			testCase.Name = filepath.Base(filePath)
+			var err error
+			testCase.Address, err = GetFixedAddress(parsedFileContent.Components, suite.Config) // OpenCageData's expected output relies on fixed addresses
+			suite.NoError(err, "GetFixedAddress failed for test case %s", testCase.Name)
+			suite.TestCases = append(suite.TestCases, testCase)
+		}
+	}
+}
+
+func (suite *FormatterTestSuite) TestAddressFormatter() {
+	for _, testCase := range suite.TestCases {
+		formattedAddress, err := FormatAddress(testCase.Address, suite.Config)
+
+		suite.NoError(err)
+		suite.Equalf(testCase.ExpectedOutput, formattedAddress, "Test case file: %s", testCase.Name)
+	}
+}
